aaed0b14b07d234b1912044929acc8d873c9427e
Rodeobazaar124 <azfasa15@gmail.com>
2024-07-10 20:51:37 +0700

    Initialize ecosystem config for PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps: [
+        {
+            name: 'smkn2sumedang',
+            script: 'node_modules/next/dist/bin/next',
+            args: 'start',
+            env_local: {
+                APP_ENV: 'local' // APP_ENV=local
+            },
+            env_dev: {
+                APP_ENV: 'dev' // APP_ENV=dev
+            },
+            env_prod: {
+                APP_ENV: 'prod' // APP_ENV=prod
+            }
+        }
+    ]
+}
